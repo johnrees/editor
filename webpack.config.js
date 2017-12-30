d6338c992f99d47d8ba13e8d8a4834b650b76cc5
John Rees <johnrees@users.noreply.github.com>
2017-12-30 21:54:03 +0000

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  devtool: "source-map", // - Approach that plays well with UglifyJsPlugin; typically you might use this in Production
+  context: path.resolve(__dirname, "./src"),
+  entry: {
+    app: "./index.tsx"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "./dist/js/"),
+    publicPath: "/js/"
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "./public"),
+    hot: false,
+    inline: false,
+    host: '0.0.0.0'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      THREE: "three"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: "babel-loader",
+            options: { presets: ["env"] }
+          },
+          {
+            loader: "ts-loader"
+          }
+        ]
+      },
+      {
+        test: /\.worker\.js$/,
+        use: { loader: 'worker-loader' }
+      }
+    ]
+  }
+};
